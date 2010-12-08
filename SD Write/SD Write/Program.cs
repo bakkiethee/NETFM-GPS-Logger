09320cb849d7f879c60e42eaaff8b56be32875f4
bakkiethee <bakkiethee@quicknet.nl>
2010-12-08 16:52:15 +0100

    First Projects

---
+++ SD Write/SD Write/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Threading;
+using System.Text;
+using Microsoft.SPOT;
+using System.IO;
+using Microsoft.SPOT.IO;
+using GHIElectronics.NETMF.IO;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF.FEZ;
+using System;
+
+// /*
+namespace MFConsoleApplication1
+{
+public class Program
+{
+static void Main()
+{
+    
+    // ... check if SD is inserted
+    // SD Card is inserted
+    // Create a new storage device
+    PersistentStorage sdPS = new PersistentStorage("SD");
+    // Mount the file system
+    sdPS.MountFileSystem();
+    // Assume one storage device is available,
+    // access it through NETMF
+    string rootDirectory = VolumeInfo.GetVolumes()[0].RootDirectory;
+    FileStream FileHandle = new FileStream(rootDirectory + @"\hello1.txt", FileMode.Create);
+    byte[] data = Encoding.UTF8.GetBytes("This string will go in the file!");
+      // Toggle LED on SD Write
+    OutputPort LED;
+    LED = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.LED, true);
+    LED.Write(true); 
+        // write the data and close the file
+    FileHandle.Write(data, 0, data.Length);
+          FileHandle.Close();
+    // Turn off led
+    LED.Write(false);
+    // if we need to unmount
+    sdPS.UnmountFileSystem();
+    // ...
+    Thread.Sleep(100);
+}
+}
+}
+// */
+
+/*
+namespace MFConsoleApplication1
+{
+public class Program
+{
+static void Main()
+{
+// ... check if SD is inserted
+// SD Card is inserted
+// Create a new storage device
+PersistentStorage sdPS = new PersistentStorage("SD");
+// Mount the file system
+sdPS.MountFileSystem();
+// Assume one storage device is available,
+// access it through NETMF
+string rootDirectory = VolumeInfo.GetVolumes()[0].RootDirectory;
+FileStream FileHandle = new FileStream(rootDirectory +
+@"\hello.txt", FileMode.Open, FileAccess.Read);
+byte[] data = new byte[100];
+// write the data and close the file
+int read_count = FileHandle.Read(data, 0, data.Length);
+FileHandle.Close();
+Debug.Print("The size of data we read is: " +
+read_count.ToString());
+Debug.Print("Data from file:");
+Debug.Print(new string(Encoding.UTF8.GetChars(data), 0,
+read_count));
+// if we need to unmount
+sdPS.UnmountFileSystem();
+// ...
+Thread.Sleep(Timeout.Infinite);
+}
+}
+}
+*/
---
+++ SDDirScan/SDDirScan/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.IO;
+using GHIElectronics.NETMF.IO;
+namespace Test
+{
+class Program
+{
+public static void Main()
+{
+// ...
+// SD Card is inserted
+// Create a new storage device
+PersistentStorage sdPS = new PersistentStorage("SD");
+// Mount the file system
+sdPS.MountFileSystem();
+    // Assume one storage device is available, access it through
+// Micro Framework and display available files and folders:
+Debug.Print("Getting files and folders:");
+if (VolumeInfo.GetVolumes()[0].IsFormatted)
+{
+string rootDirectory =
+VolumeInfo.GetVolumes()[0].RootDirectory;
+string[] files = Directory.GetFiles(rootDirectory);
+string[] folders = Directory.GetDirectories(rootDirectory);
+Debug.Print("Files available on " + rootDirectory + ":");
+for (int i = 0; i < files.Length; i++)
+Debug.Print(files[i]);
+Debug.Print("Folders available on " + rootDirectory + ":");
+for (int i = 0; i < folders.Length; i++)
+Debug.Print(folders[i]);
+}
+else
+{
+Debug.Print("Storage is not formatted. Format on PC with FAT32/FAT16 first.");
+}
+// Unmount
+sdPS.UnmountFileSystem();
+}
+}
+}
